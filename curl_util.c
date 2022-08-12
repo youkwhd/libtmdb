98ef5a5b6fc52c955eacbc6bda5fc44df49230a1
youkwhd <lolywk@tutanota.com>
2022-08-12 16:28:48 +0700

    refactor: rename buf to membuf

--- curl_util.c
+++ curl_util.c
@@ -1,9 +1,9 @@
 #include "curl_util.h"
 
-size_t curl_writefunction_callback(char *data, size_t size, size_t nmemb, membuffer *buf)
+size_t curl_writefunction_callback(char *data, size_t size, size_t nmemb, membuffer *membuf)
 {
     size_t bytes = size * nmemb;
-    membuf_append(buf, data, bytes);
+    membuf_append(membuf, data, bytes);
 
     return bytes; 
 }
--- curl_util.h
+++ curl_util.h
@@ -4,6 +4,6 @@
 #include <stddef.h>
 #include "membuffer.h"
 
-size_t curl_writefunction_callback(char *data, size_t size, size_t nmemb, membuffer *buf);
+size_t curl_writefunction_callback(char *data, size_t size, size_t nmemb, membuffer *membuf);
 
 #endif /* __CURL_UTIL_H */
