9380079c73d976612089b1e0e820200810edfd24
youkwhd <lolywk@tutanota.com>
2023-06-11 18:56:40 +0700

    fix: path should start with /

--- account.c
+++ account.c
@@ -8,7 +8,7 @@ TMDb_Buffer *tmdb_get_account_details(TMDb_Query *query)
     }
 
     char path[256];
-    sprintf(path, "3/account/%s", tmdb_query_get(query, "account_id"));
+    sprintf(path, "/3/account/%s", tmdb_query_get(query, "account_id"));
     return tmdb_request_create_get(query, (const char *[]){"account_id"}, 1, path);
 }
 
@@ -20,7 +20,7 @@ TMDb_Buffer *tmdb_get_account_lists(TMDb_Query *query)
     }
 
     char path[256];
-    sprintf(path, "3/account/%s/lists", tmdb_query_get(query, "account_id"));
+    sprintf(path, "/3/account/%s/lists", tmdb_query_get(query, "account_id"));
     return tmdb_request_create_get(query, (const char *[]){"account_id"}, 1, path);
 }
 
@@ -32,7 +32,7 @@ TMDb_Buffer *tmdb_get_account_favorite_movies(TMDb_Query *query)
     }
 
     char path[256];
-    sprintf(path, "3/account/%s/movies", tmdb_query_get(query, "account_id"));
+    sprintf(path, "/3/account/%s/movies", tmdb_query_get(query, "account_id"));
     return tmdb_request_create_get(query, (const char *[]){"account_id"}, 1, path);
 }
 
@@ -44,48 +44,20 @@ TMDb_Buffer *tmdb_get_account_favorite_tv(TMDb_Query *query)
     }
 
     char path[256];
-    sprintf(path, "3/account/%s/favorite/movies", tmdb_query_get(query, "account_id"));
+    sprintf(path, "/3/account/%s/favorite/movies", tmdb_query_get(query, "account_id"));
     return tmdb_request_create_get(query, (const char *[]){"account_id"}, 1, path);
 }
 
 TMDb_Buffer *tmdb_post_account_mark_favorite(TMDb_Query *query, const char *request_body)
 {
-    /* Check for required queries
-     */
     if (!tmdb_query_has(query, "account_id")) {
         tmdb_query_cleanup(query);
         return NULL;
     }
-    char si_query[256] = "session_id=";
 
-    TMDb_Buffer *membuf = membuf_init(1024 * sizeof(char));
-    curl_easy_setopt(__global_tmdb_config.curl_handler, CURLOPT_WRITEDATA, membuf);
-
-    struct curl_slist *header = NULL;
-    header = curl_slist_append(header, "Content-Type: application/json;charset=utf-8");
-    curl_easy_setopt(__global_tmdb_config.curl_handler, CURLOPT_HTTPHEADER, header);
-    curl_easy_setopt(__global_tmdb_config.curl_handler, CURLOPT_POSTFIELDS, request_body);
-
-    CURLU *url = tmdb_url_init();
-    if (url == NULL) return NULL;
-    if (tmdb_url_query_append(url, si_query, session_id) != CURLUE_OK) return NULL;
-
-    char path[256] = "/3/account/";
-    strcat(path, account_id);
-    strcat(path, "/favorite");
-
-    CURLUcode uc = curl_url_set(url, CURLUPART_PATH, path, 0);
-    if (uc != CURLUE_OK) return NULL;
-
-    CURLcode res = curl_easy_perform(__global_tmdb_config.curl_handler);
-    if (res != CURLE_OK) return NULL;
-
-    curl_easy_setopt(__global_tmdb_config.curl_handler, CURLOPT_HTTPHEADER, NULL);
-    curl_easy_setopt(__global_tmdb_config.curl_handler, CURLOPT_POST, 0);
-    curl_slist_free_all(header);
-
-    tmdb_url_cleanup(url);
-    return membuf;
+    char path[256];
+    sprintf(path, "/3/account/%s/favorite", tmdb_query_get(query, "account_id"));
+    return tmdb_request_create_post(query, (const char *[]){"account_id"}, 1, path, request_body);
 }
 
 TMDb_Buffer *tmdb_get_account_rated_movies(TMDb_Query *query)
@@ -96,7 +68,7 @@ TMDb_Buffer *tmdb_get_account_rated_movies(TMDb_Query *query)
     }
 
     char path[256];
-    sprintf(path, "3/account/%s/rated/movies", tmdb_query_get(query, "account_id"));
+    sprintf(path, "/3/account/%s/rated/movies", tmdb_query_get(query, "account_id"));
     return tmdb_request_create_get(query, (const char *[]){"account_id"}, 1, path);
 }
 
@@ -108,7 +80,7 @@ TMDb_Buffer *tmdb_get_account_rated_tv(TMDb_Query *query)
     }
 
     char path[256];
-    sprintf(path, "3/account/%s/rated/tv", tmdb_query_get(query, "account_id"));
+    sprintf(path, "/3/account/%s/rated/tv", tmdb_query_get(query, "account_id"));
     return tmdb_request_create_get(query, (const char *[]){"account_id"}, 1, path);
 }
 
@@ -120,7 +92,7 @@ TMDb_Buffer *tmdb_get_account_rated_tv_episodes(TMDb_Query *query)
     }
 
     char path[256];
-    sprintf(path, "3/account/%s/rated/tv/episodes", tmdb_query_get(query, "account_id"));
+    sprintf(path, "/3/account/%s/rated/tv/episodes", tmdb_query_get(query, "account_id"));
     return tmdb_request_create_get(query, (const char *[]){"account_id"}, 1, path);
 }
 
@@ -132,7 +104,7 @@ TMDb_Buffer *tmdb_get_account_movie_watchlist(TMDb_Query *query)
     }
 
     char path[256];
-    sprintf(path, "3/account/%s/watchlist/movies", tmdb_query_get(query, "account_id"));
+    sprintf(path, "/3/account/%s/watchlist/movies", tmdb_query_get(query, "account_id"));
     return tmdb_request_create_get(query, (const char *[]){"account_id"}, 1, path);
 }
 
@@ -146,7 +118,7 @@ TMDb_Buffer *tmdb_get_account_tv_watchlist(TMDb_Query *query)
     }
 
     char path[256];
-    sprintf(path, "3/account/%s/watchlist/tv", tmdb_query_get(query, "account_id"));
+    sprintf(path, "/3/account/%s/watchlist/tv", tmdb_query_get(query, "account_id"));
     return tmdb_request_create_get(query, (const char *[]){"account_id"}, 1, path);
 }
 
@@ -156,34 +128,8 @@ TMDb_Buffer *tmdb_post_account_add_watchlist(TMDb_Query *query, const char *requ
         tmdb_query_cleanup(query);
         return NULL;
     }
-    char si_query[256] = "session_id=";
 
-    TMDb_Buffer *membuf = membuf_init(1024 * sizeof(char));
-    curl_easy_setopt(__global_tmdb_config.curl_handler, CURLOPT_WRITEDATA, membuf);
-
-    struct curl_slist *header = NULL;
-    header = curl_slist_append(header, "Content-Type: application/json;charset=utf-8");
-    curl_easy_setopt(__global_tmdb_config.curl_handler, CURLOPT_HTTPHEADER, header);
-    curl_easy_setopt(__global_tmdb_config.curl_handler, CURLOPT_POSTFIELDS, request_body);
-
-    CURLU *url = tmdb_url_init();
-    if (url == NULL) return NULL;
-    if (tmdb_url_query_append(url, si_query, session_id) != CURLUE_OK) return NULL;
-
-    char path[256] = "/3/account/";
-    strcat(path, account_id);
-    strcat(path, "/watchlist");
-
-    CURLUcode uc = curl_url_set(url, CURLUPART_PATH, path, 0);
-    if (uc != CURLUE_OK) return NULL;
-
-    CURLcode res = curl_easy_perform(__global_tmdb_config.curl_handler);
-    if (res != CURLE_OK) return NULL;
-
-    curl_easy_setopt(__global_tmdb_config.curl_handler, CURLOPT_HTTPHEADER, NULL);
-    curl_easy_setopt(__global_tmdb_config.curl_handler, CURLOPT_POST, 0);
-    curl_slist_free_all(header);
-
-    tmdb_url_cleanup(url);
-    return membuf;
+    char path[256];
+    sprintf(path, "/3/account/%s/watchlist", tmdb_query_get(query, "account_id"));
+    return tmdb_request_create_post(query, (const char *[]){"account_id"}, 1, path, request_body);
 }
--- account.h
+++ account.h
@@ -11,12 +11,12 @@ TMDb_Buffer *tmdb_get_account_details(TMDb_Query *query);
 TMDb_Buffer *tmdb_get_account_lists(TMDb_Query *query);
 TMDb_Buffer *tmdb_get_account_favorite_movies(TMDb_Query *query);
 TMDb_Buffer *tmdb_get_account_favorite_tv(TMDb_Query *query);
-TMDb_Buffer *tmdb_post_account_mark_favorite(TMDb_Query *query);
+TMDb_Buffer *tmdb_post_account_mark_favorite(TMDb_Query *query, const char *request_body);
 TMDb_Buffer *tmdb_get_account_rated_movies(TMDb_Query *query);
 TMDb_Buffer *tmdb_get_account_rated_tv(TMDb_Query *query);
 TMDb_Buffer *tmdb_get_account_rated_tv_episodes(TMDb_Query *query);
 TMDb_Buffer *tmdb_get_account_movie_watchlist(TMDb_Query *query);
 TMDb_Buffer *tmdb_get_account_tv_watchlist(TMDb_Query *query);
-TMDb_Buffer *tmdb_post_account_add_watchlist(TMDb_Query *query);
+TMDb_Buffer *tmdb_post_account_add_watchlist(TMDb_Query *query, const char *request_body);
 
 #endif
